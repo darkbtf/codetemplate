2d7c4cc9ffa58023a8d535700b1c4cafbf641582
Guest <guest-lmUylV@pclab67.(none)>
2014-10-25 13:13:01 +0800

    default

---
+++ default.cpp
@@ -0,0 +1,35 @@
+//by david942j
+#include <bits/stdc++.h>
+#define openfile(s) freopen(s".in","r",stdin);freopen(s".out","w",stdout)
+#define mpr std::make_pair
+#define lg std::__lg
+#define __count __builtin_popcount
+#define X first
+#define Y second
+#define mst(x) memset(x,0,sizeof(x))
+#define mst1(x) memset(x,-1,sizeof(x))
+#define ALL(c) (c).begin(),(c).end()
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FOR1(i,n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define RID(x) int x;RI(x)
+using namespace std;
+typedef long long LL;
+typedef double LD;
+typedef vector<int> VI;
+typedef std::pair<int,int> PII;
+template<class T>inline void maz(T &a,T b){if(a<b)a=b;}
+template<class T>inline void miz(T &a,T b){if(a>b)a=b;}
+template<class T>inline T abs(T a){return a>0?a:-a;}
+void RI() {}
+template<typename... T>
+void RI( int& head, T&... tail ) {
+    scanf("%d",&head);
+    RI(tail...);
+}
+const int N=100100;
+
+int main(){
+	//openfile("");
+	return 0;
+}
